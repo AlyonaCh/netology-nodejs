9b9051a3a700de7283b3d2bc166ad26e222b0f3a
Alyona <alyonacherny@gmail.com>
2022-08-19 22:01:21 +0300

    docker

--- hw1.9/counter/src/index.js
+++ hw1.9/counter/src/index.js
@@ -2,24 +2,25 @@ const express = require('express');
 const redis  = require('redis');
 const REDIS_URL = process.env.REDIS_URL || 'redis://localhost'
 
-const client = redis.createClient({url:REDIS_URL})
+const client = redis.createClient({url:REDIS_URL});
 
 (async () => {
     await client.connect();
 })
 
 const app = express();
+
 app.use('/:id', async (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
     try {
-        const cnt = await client.incr(id)
-        res.json(cnt)
+        const cnt = await client.incr(id);
+        res.json({msg:'ghbrt', cnt});
     } catch (e) {
         console.log(e);
-        res.statusCode(500).json({eror:500})
+        res.statusCode(500).json({eror:500});
     }
     
 })();
 
-const PORT = 3000;
+const PORT = 3002;
 app.listen(PORT);
