6d6adcd31b9c93ca6a24e8172bb7116646f40f33
Alyona <alyonacherny@gmail.com>
2022-08-21 17:44:45 +0300

    mistake

--- hw1.9/src/index.js
+++ hw1.9/src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
-const errorMiddleware = require('./middleware/err-404');
+const errorMiddleware = require('./middleware/err-404.js');
+
+const redis  = require('redis');
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost'
+
+const client = redis.createClient({url:REDIS_URL});
+
+(async () => {
+    await client.connect();
+})
 
 const indexRouter = require('./routes/index');
 const bookRouter = require('./routes/book');
+const counterRouter = require('./routes/counter');
 
 const app = express();
 app.use(express.urlencoded());
@@ -11,6 +21,7 @@ app.set("view engine", "ejs");
 
 app.use('/', indexRouter);
 app.use('/api/books', bookRouter);
+app.use('/counter', counterRouter);
 
 app.use(errorMiddleware);
 
